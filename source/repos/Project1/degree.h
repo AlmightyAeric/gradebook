904620162e38ff533485e215cf10a37754230c9c
eric <ericdavis007@protonmail.com>
2025-09-10 23:05:13 -0500

    added some notes as well as a static constant for array strings. did so to fix code 1 error when compiling.

---
+++ source/repos/Project1/degree.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string> 
+
+
+// Defines an enumeration for DegreeProgram with three possible values
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+// Declares an array of strings that correspond to the names of the DegreeProgram enums
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
