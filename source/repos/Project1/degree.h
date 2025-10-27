b60c04d9d06dab64845ba763dc41d8942311cbec
Eric <ericdavis00657@gmail.com>
2025-10-27 16:52:18 -0500

    Delete source directory

--- source/repos/Project1/degree.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string> 
-
-
-// Defines an enumeration for DegreeProgram with three possible values
-enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
-
-// Declares an array of strings that correspond to the names of the DegreeProgram enums
-static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
